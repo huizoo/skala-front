7827da89a8ffc534e52e8beff8924b97fbbffddd
huizoo <ajtwlsgmlwn@naver.com>
2026-07-16 15:35:50 +0900

    내 가방 보기 update

--- script/bag.js
+++ script/bag.js
@@ -1,17 +1,17 @@
 const showMyBag = () => {
-  const myBag = {
-    지갑: 1,
-    스마트폰: 1,
-    에어팟: 1,
-    볼펜: 2,
-    화장품: 3,
-  };
+  const myBag = [
+    { name: "지갑", count: 1 },
+    { name: "스마트폰", count: 1 },
+    { name: "에어팟", count: 1 },
+    { name: "볼펜", count: 2 },
+    { name: "화장품", count: 3 },
+  ];
 
   let message = "[내 가방 속 물품 목록]\n\n";
   let cnt = 0;
 
-  for (const item in myBag) {
-    message += `- ${item}: ${myBag[item]}개\n`;
+  for (const item of myBag) {
+    message += `${item.name}: ${item.count}개\n`;
     cnt++;
   }
 
